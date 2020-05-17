b0b975a4ebd7877839db6ec8c0775a419b91fea3
Chelzea Salvador <chelzeasalvador@gmail.com>
2020-05-17 23:22:12 +0800

    Pagination & Blog Page

--- app/public/wp-content/themes/velaris/index.php
+++ app/public/wp-content/themes/velaris/index.php
@@ -2,7 +2,7 @@
     get_header(); ?>
 
     <div class="page-banner">
-        <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
+        <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/campus.jpg') ?>);"></div>
         <div class="page-banner__content container container--narrow">
         <h1 class="page-banner__title">Welcome to our Blog!</h1>
         <div class="page-banner__intro">
@@ -26,6 +26,8 @@
             </div>
             </div>
         <?php }
+
+        echo paginate_links();
     ?>
     </div>
 
