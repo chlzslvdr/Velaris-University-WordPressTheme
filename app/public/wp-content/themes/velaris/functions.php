e7db69e09f0d69ca39b35b233a7f38fee1cd125a
Chelzea Salvador <chelzeasalvador@gmail.com>
2020-05-29 13:44:10 +0800

    REST API: Add New Custom Field

--- app/public/wp-content/themes/velaris/functions.php
+++ app/public/wp-content/themes/velaris/functions.php
@@ -1,4 +1,12 @@
 <?php
+
+    function univeristy_custom_rest() {
+        register_rest_field('post', 'authorName', array(
+            'get_callback' => function() {return get_the_author();}
+        ));
+    }
+    add_action('rest_api_init', 'univeristy_custom_rest');
+
     function pageBanner($args = NULL) {
     
         if (!$args['title']) {
