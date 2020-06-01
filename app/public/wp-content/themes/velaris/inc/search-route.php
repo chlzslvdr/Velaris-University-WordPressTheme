4bfd6cf157a9a880f54fb434059e37d82a4a1df5
Chelzea Salvador <chelzeasalvador@gmail.com>
2020-06-01 20:09:49 +0800

    Completing Our Search Overlay

--- app/public/wp-content/themes/velaris/inc/search-route.php
+++ app/public/wp-content/themes/velaris/inc/search-route.php
@@ -81,23 +81,42 @@ function universityRegisterSearch() {
         }
 
         $programRelationshipQuery = new WP_Query(array(
-        'post_type' => 'professor',
+        'post_type' => array('professor', 'event'),
         'meta_query' => $programsMetaQuery
         ));
 
         while($programRelationshipQuery->have_posts()) {
         $programRelationshipQuery->the_post();
 
-        if (get_post_type() == 'professor') {
-            array_push($results['professors'], array(
-            'title' => get_the_title(),
-            'permalink' => get_the_permalink(),
-            'image' => get_the_post_thumbnail_url(0, 'professorLandscape')
+        if (get_post_type() == 'event') {
+            $eventDate = new DateTime(get_field('event_date'));
+            $description = null;
+            if (has_excerpt()) {
+                $description = get_the_excerpt();
+            } else {
+                $description = wp_trim_words(get_the_content(), 16);
+            }
+    
+            array_push($results['events'], array(
+                'title' => get_the_title(),
+                'permalink' => get_the_permalink(),
+                'month' => $eventDate->format('M'),
+                'day' => $eventDate->format('d'),
+                'description' => $description
             ));
-        }
+            }
+
+            if (get_post_type() == 'professor') {
+                array_push($results['professors'], array(
+                'title' => get_the_title(),
+                'permalink' => get_the_permalink(),
+                'image' => get_the_post_thumbnail_url(0, 'professorLandscape')
+                ));
+            }
         }
 
         $results['professors'] = array_values(array_unique($results['professors'], SORT_REGULAR));
+        $results['events'] = array_values(array_unique($results['events'], SORT_REGULAR));
     }
 
     return $results;
